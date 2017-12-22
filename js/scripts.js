e9df9e450edb7c4c7dda3de7ba628d436fe01393
Krzysztof Sliwinski <kpsliwinski@gmail.com>
2017-12-22 13:15:04 +0100

    Correct js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 // scripts.js
+var prefix = "https://cors-anywhere.herokuapp.com/";
+
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
